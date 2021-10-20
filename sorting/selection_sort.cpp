94601e85f7ba6a2f4f490a6e35414bce23f0b7c8
Manish Shambhoo Sah <msah74915@gmail.com>
2021-10-21 01:29:03 +0530

    implemented selection sort

---
+++ sorting/selection_sort.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+void InsertionSort();
+
+int main(){
+    do{
+        cout<<"1. Insert\n";
+        cout<<"2. Quit\n";
+        int choice ;
+        cin>>choice;
+        switch(choice){
+            case 1:InsertionSort();
+            break;
+            case 2:
+                exit(1);
+            break;
+        }
+    }while(1);
+    return 0;
+}
+void InsertionSort(){
+    int n;
+    cout<<"How many numbers of value to sort : ";
+    cin>>n;
+    cout<<"\n Enter the value : ";
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    cout<<"\nIntial Stage : ";
+    for(int i=0; i<n; i++){
+        cout<<a[i]<<" ";
+    }cout<<endl;
+        int j,k,temp;
+    for(int i=0; i<n; i++){
+        j = k = i;
+        for(j; j<n; j++){
+            if(a[j]<a[k]){
+                k = j;
+            }
+        }
+        temp = a[i];
+        a[i] = a[k];
+        a[k] = temp;
+    }
+    cout<<"final Stage : ";
+    for(int i=0; i<n; i++){
+        cout<<a[i]<<" ";
+    }cout<<endl;
+}
